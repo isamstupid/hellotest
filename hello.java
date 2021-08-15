695107f51748e79a1d4c72e3dfd987e68b8d82f4
isamstupid <samidha.salian2gmail.com>
2021-08-15 20:52:42 +0530

    added hello

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello{
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=10 ;i++ ) {
+			System.out.println(" Hello world ....... Count :"+ i);
+		}
+		System.out.println("Program ends here !");
+	}
+}
